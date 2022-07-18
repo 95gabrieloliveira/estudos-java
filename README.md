5857c66e4b0fa83badfd6a576145b70421123b8e
Gabriel Oliveira <95gabrieloliveira@gmail.com>
2022-07-18 20:53:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# estudos-java
+ Estudos da Imersão java da Alura
---
+++ app.java
@@ -0,0 +1,13 @@
+public class app {
+    public static void main (String[] args) throws Exception 
+    {
+        System.out.println("Hello, World!");
+
+        // Fazer uma conexão HTTP e Buscar os top 250 FIlmes
+        String url = "api.com/en/API/Top250Movies/k_xz3krpo1";
+        
+        // Extrair só os dados que interessam (titulo, poster, classificação)
+
+        // exibir e manipular os dados
+    }
+}
