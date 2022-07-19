97b8890974f5ac63c781c9b751989842adebfa75
Gabriel Oliveira <95gabrieloliveira@gmail.com>
2022-07-18 21:30:32 -0300

    Inicio

--- app.java
+++ app.java
@@ -1,11 +1,17 @@
+import java.net.URI;
+import java.net.http.httpClient;
+
 public class app {
     public static void main (String[] args) throws Exception 
+
     {
         System.out.println("Hello, World!");
 
         // Fazer uma conexão HTTP e Buscar os top 250 FIlmes
-        String url = "api.com/en/API/Top250Movies/k_xz3krpo1";
-        
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_xz3krpo1";
+        URI endereco  URI.create(url);
+        var client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         // Extrair só os dados que interessam (titulo, poster, classificação)
 
         // exibir e manipular os dados
